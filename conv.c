24d96b133a0e7c64d3b37ceebc0d31a98d079d38
pasalapati <100088906+pasalapati@users.noreply.github.com>
2022-07-05 10:14:43 +0530

    Update conv.c

--- conv.c
+++ conv.c
@@ -80,49 +80,6 @@ float * conv(int input[][IMG][IMG],int filter[][FIL][FIL],int stride,int bias){
 		}
     	printf("\n");
 	}	
-	int *pool_out = pool(out,1);
+	float *pool_out = pool(out,1);
 	return out;
 }
-float * pool(float input[][3],int stride)
-{
-	int s=stride,t=0,k=0,g,x=0,y=0,a,b,max=0;
-	float outpool[2][2],f=0;
-	for(g=0;g<4;g++){
-		int l=0;
-		if(g%2==1){
-			f=k=l=1;
-		}
-		if(g>1){
-			f=1;
-		}
-		for(a=f;a<2+f;a++)
-		{
-			for(b=l;b<2+l;b++)
-			{	
-				if(input[a][b]>max)
-				{
-					outpool[x][y]=input[a][b];
-					max=outpool[x][y];
-				}
-			}
-			
-		}
-		y++;
-		t++;
-		max=0;
-	}
-printf("\n\n");
-	for(x=0;x<2;x++)
-	{
-		for(y=0;y<2;y++)
-		{
-		
-			printf(" %f ",outpool[x][y]);
-		}
-		//printf("\n");
-	}
-	return outpool;
-}
-
-
-
